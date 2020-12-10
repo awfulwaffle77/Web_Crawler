7b0520184d75b77146541c7f3754010f7c936dc0
awfulwaffle <heroidorin@gmail.com>
2020-12-10 15:06:35 +0200

    Modificare functionalitate PageInterpreter
    Issue #14
    Am adaugat functionalitati in functiile formatPages si isPageCorrect
    
    Signed-off-by: awfulwaffle <heroidorin@gmail.com>

--- src/ro/mta/PageInterpreter.java
+++ src/ro/mta/PageInterpreter.java
@@ -4,18 +4,22 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PageInterpreter {
-    private Map<String, String> rules;
-    List<String> lines;
+    private Map<String, String> rules = new HashMap<>();
+    private List<String> lines;
+    private int maxRules = 0;
 
     public PageInterpreter(String filename) throws IOException {
         try {
             lines = Files.readAllLines(Paths.get(filename), StandardCharsets.UTF_8);
+
             for(String line: lines){
                 rules.put(line.split(":")[0], line.split(":")[1]);
+                maxRules++;
             }
         }
         catch (IOException e){
@@ -23,15 +27,67 @@ public class PageInterpreter {
         }
     }
 
-    boolean isPageCorrect(List<String> htmlString){
+    boolean isPageCorrect(String htmlString){ // if all rules apply, returns true
+        int correctiveness = 0;
+
        if(rules.get("contains") != null){
-          String val = rules.get("contains");
+           // contains:cerc,mta,romania
+           StringBuilder toMatch = new StringBuilder();
+           String finalMatch;
+           for(String x : rules.get("contains").split(",")){
+               toMatch.append(x);
+               toMatch.append('|');
+           }
+           finalMatch = toMatch.substring(0,toMatch.length()-1);
+           String patternString = "\\b(" + finalMatch + ")\\b";
+           Pattern pattern = Pattern.compile(patternString);
+
+           Matcher matcher = pattern.matcher(htmlString);
+
+           if (matcher.find()) { // finds at least one of the words
+               correctiveness++;
+           }
+
+       }
+       if(rules.get("max_lines") != null){
+           int maxSize = Integer.parseInt(rules.get("max_lines"));
+           int lines = htmlString.split("\r\n|\r|\n").length;
+
+           if(lines < maxSize){
+               correctiveness++;
+           }
+
        }
 
-       return true;
+        return correctiveness == maxRules;
     }
 
-    String formatPages(){
-        return "formatted";
+    String formatPages(String htmlString){
+        StringBuilder finalPage = new StringBuilder();
+
+        String[] splittedHtmlString = htmlString.split("\r\n|\r|\n");
+        int maxLines = splittedHtmlString.length;
+
+        if(rules.get("max_lines") != null){
+            maxLines = Integer.parseInt(rules.get("max_lines"));
+        }
+
+        String[] maxLinedString = new String[maxLines];
+        for(int i = 0; i < maxLines; i++){
+            maxLinedString[i] = splittedHtmlString[i];
+        }
+
+        if(rules.get("contains") != null){
+            String[] containsList = rules.get("contains").split(",");
+            for(String line : maxLinedString){
+              for(String element : containsList){
+                  if(line.contains(element)){
+                      finalPage.append(line);
+                      break;
+                  }
+              }
+          }
+       }
+        return finalPage.toString();
     }
 }
