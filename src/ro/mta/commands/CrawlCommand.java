3146c141336c0f5eb392b0ca2a1d493c66be21a5
Mihai <mihai.paun@mta.ro>
2020-12-12 22:04:56 +0200

    Update CrawlCommand.java
    Am implementat clasa CrawlCommand.java

--- src/ro/mta/commands/CrawlCommand.java
+++ src/ro/mta/commands/CrawlCommand.java
@@ -1,13 +1,84 @@
 package ro.mta.commands;
 
+import ro.mta.URLDownloader;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * crawl command implements the crawling and downloading of the selected urls
+ * command syntax: crawler crawl config.conf url.txt
+ * - config.conf - configuration file for the crawler
+ * - url.txt - text file which contains the web sites to be downloaded
+ */
+
 public class CrawlCommand extends AbstractCommand {
+
+    private String configFile;
+    /**
+     * config.conf:
+     * root_dir
+     * max_nb_of_pages
+     */
+    private String urlFile;
+
+    private String rootDir;
+    private int nbOfPages;
+
+    private ArrayList<String> urls;
+
+    /**
+     *
+     * @param args - args[1] - config file name
+     *             - args[2] - file which contains url to be crawled
+     */
 
     public CrawlCommand(String[] args) {
         super(args);
+        configFile = args[1];
+        urlFile = args[2];
+
+        try{
+            System.out.println("Working Directory = " + System.getProperty("user.dir"));
+            BufferedReader reader = new BufferedReader(new FileReader(configFile));
+            String line;
+
+            line = reader.readLine();
+            rootDir = line;
+
+            line = reader.readLine();
+            nbOfPages = Integer.parseInt(line);
+
+            reader.close();
+            reader = new BufferedReader(new FileReader(urlFile));
+
+            urls = new ArrayList<String>();
+
+            while ((line = reader.readLine()) != null){
+                urls.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.out.println(ioe.getMessage());
+        }
     }
 
     @Override
     public void execute() {
-
+        try{
+            URLDownloader u = new URLDownloader(rootDir,nbOfPages);
+            for(Iterator<String> it=urls.iterator(); it.hasNext();){
+                u.download(it.next(),0);
+            }
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+            System.out.println(ioe.getMessage());
+        }
     }
 }
