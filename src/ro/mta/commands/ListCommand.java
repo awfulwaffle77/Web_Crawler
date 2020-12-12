50566478ee4f3fa9feb3d1fea9c9356e55d71fd9
Bogdan849 <ilie.bogdan.constantin2@gmail.com>
2020-12-13 00:17:17 +0200

    Functionalitatea List
    Implementare functionalitate List
    
    Issue: #35
    S-a implementat functionalitatea de listare a fisierelor cu oanumita extensie, introdusa in program ca argument. Calea rezultata este cea absoluta, pentru un rezultat concret.
    
    Signed-off-by: Ilie Bogdan-Constantin
    <ilie.bogdan.constantin2@gmail.com>

--- src/ro/mta/commands/ListCommand.java
+++ src/ro/mta/commands/ListCommand.java
@@ -1,14 +1,81 @@
 package ro.mta.commands;
 
-import java.util.List;
+
+import javax.naming.directory.InvalidSearchFilterException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.NotDirectoryException;
+import java.nio.file.Path;
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;//folosesc regex ca sa identific sfarsitul fisierului
+
 
 public class ListCommand extends AbstractCommand {
-    public ListCommand(List<String> args) {
+
+    private String argument;
+    private String pathToDir;
+    private Boolean fileFound;
+
+    private String validatePathToDir(String pathToDir) throws NotDirectoryException {
+        if (Files.isDirectory(Path.of(pathToDir))){
+            return pathToDir;
+        }
+        else {
+            throw new NotDirectoryException("The path given to the directory is not good");
+        }
+    }
+
+    private String checkListingArg(String listingArg) throws InvalidSearchFilterException {
+        Pattern pattern = Pattern.compile("[a-z0-9]", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(listingArg);
+        if (matcher.find()){
+            return listingArg;
+        }
+        else {
+            throw new InvalidSearchFilterException("The listing extension is not valid.");
+        }
+    }
+
+    public ListCommand(String[] args) {
         super(args);
+        try{
+            fileFound = false;
+            pathToDir = validatePathToDir(args[1]);
+            argument = checkListingArg(args[2]);
+        } catch (NotDirectoryException | InvalidSearchFilterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void searchAndList( String path ) {
+
+        Pattern pattern = Pattern.compile("(.*?)" + "\\." + argument+"$", Pattern.CASE_INSENSITIVE);
+
+        File root = new File( path );
+        File[] list = root.listFiles();
+
+        if (list == null) return;
+
+        for ( File f : list ) {
+            if ( f.isDirectory() ) {
+                searchAndList( f.getAbsolutePath() ); //aici incepe recursivitatea
+            }
+            else {
+                Matcher matcher = pattern.matcher(f.getName());
+                boolean matchFound = matcher.find();
+                if(matchFound) {
+                    System.out.println(f.getAbsoluteFile());
+                    fileFound = Boolean.TRUE;
+                }
+            }
+        }
+        if(!fileFound)
+            System.out.println("Nu s-a gasit niciun fisier cu extensia data.");
     }
 
     @Override
     public void execute() {
-
+        searchAndList(pathToDir);
     }
 }
